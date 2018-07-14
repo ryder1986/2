6bca4027f945a79c969eeace0dae08cd1a446825
root <you@example.com>
2018-07-14 13:45:43 +0800

    1

---
+++ v7-tiny/camera.h
@@ -0,0 +1,98 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include "tool.h"
+#include "videosource.h"
+#include "datapacket.h"
+#include "jsondatadealer.h"
+#include "detectregion.h"
+class CameraData:public JsonData
+{
+public:
+    string url;
+    vector <DetectRegionData >detect_regions;
+    CameraData(DataPacket pkt):JsonData(pkt)
+    {
+        DECODE_STRING_MEM(url);
+        DECODE_OBJ_ARRAY_MEM(detect_regions);
+    }
+    CameraData()
+    {
+
+    }
+};
+
+class Camera:public VdData<CameraData>
+{
+    function <void(Camera *,const char *,int)>callback_result;
+public:
+    Camera(DataPacket cfg,function <void(Camera *,const char *,int)>fc):VdData(cfg),quit(false),callback_result(fc)
+    {
+
+        //    set_config(cfg);
+        for(DetectRegionData p:private_data.detect_regions){
+
+            drs.push_back(new DetectRegion(p));
+            //  pros.push_back(new PvdC4Processor(p));
+            //        pros.push_back(new PvdMvncProcessor(p.get_pkt("pvd_c4")));
+            // pros.push_back(new PvdHogProcessor(p.get_pkt("pvd_c4")));
+        }
+        src=new VideoSource(private_data.url);
+        start();
+    }
+    Camera(CameraData cfg,function <void(Camera *,const char *,int)>fc):VdData(cfg),quit(false),callback_result(fc)
+    {
+
+        //    set_config(cfg);
+        for(DetectRegionData p:private_data.detect_regions){
+               drs.push_back(new DetectRegion(p));
+            // if(GET_STRING_VALUE_FROM_PKT(selected_alg,p)=="pvd_c4")
+            //pros.push_back(new PvdC4Processor(p.get_pkt("pvd_c4")));
+            //  pros.push_back(new PvdC4Processor(p));
+            //        pros.push_back(new PvdMvncProcessor(p.get_pkt("pvd_c4")));
+            // pros.push_back(new PvdHogProcessor(p.get_pkt("pvd_c4")));
+
+        }
+        src=new VideoSource(private_data.url);
+        start();
+    }
+    ~Camera()
+    {
+        for(DetectRegion *pro:drs)
+            delete pro;
+        drs.clear();
+    }
+
+    void run_process()
+    {
+        Mat frame;
+        while(!quit){
+            this_thread::sleep_for(chrono::milliseconds(10));
+            if(src->get_frame(frame)){
+              //  prt(info,"get a frame ");
+                for(DetectRegion *r:drs){
+                    r->work(frame);
+                }
+            }
+        }
+    }
+
+    void start()
+    {
+        //   _start(bind(&TestProcess::run_process,this));
+        //  _start(bind(&TestProcess::run_process,this,placeholders::_1),99);
+        _start_async(bind(&Camera::run_process,this));
+        prt(info,"start done ~~~~~~~~~~~~~~~");
+    }
+
+    void stop()
+    {
+        quit=true;
+    }
+
+private:
+    vector<DetectRegion*> drs;
+    VideoSource *src;
+    bool quit;
+};
+
+#endif // CAMERA_H
---
+++ v7-tiny/detectregion.h
@@ -0,0 +1,69 @@
+#ifndef DETECTREGION_H
+#define DETECTREGION_H
+
+#include "jsondatadealer.h"
+#include "datapacket.h"
+
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+using namespace std;
+using namespace cv;
+
+class VdPoint:public JsonData
+{
+public:
+    int x;
+    int y;
+    VdPoint(DataPacket pkt):JsonData(pkt)
+    {
+        DECODE_INT_MEM(x);
+        DECODE_INT_MEM(y);
+    }
+    VdPoint(int x,int y)
+    {
+
+    }
+};
+class DetectRegionData:public JsonData
+{
+
+public:
+    int region_id;
+    string detector_type;
+    vector <VdPoint>poly_vers;
+    //PolyVers poly_vers;
+    DetectRegionData(DataPacket pkt):JsonData(pkt)
+    {
+        DECODE_INT_MEM(region_id);
+        DECODE_OBJ_ARRAY_MEM(poly_vers);
+        DECODE_STRING_MEM(detector_type);
+    }
+
+};
+
+class DetectRegion : public VdData<DetectRegionData>
+{
+public:
+    DetectRegion(DataPacket pkt):VdData(pkt)
+    {
+
+    }
+    DetectRegion(DetectRegionData pkt):VdData(pkt)
+    {
+        for(VdPoint p:private_data.poly_vers)
+        {
+            prt(info,"(%d,%d) ",p.x,p.y);
+        }
+    }
+    void work(Mat frame)
+    {
+
+    }
+};
+
+#endif // DETECTREGION_H
