103d46c21fe373f7adc1ad8aef8a512cfeab6ef8
root <you@example.com>
2018-08-21 18:04:20 +0800

    1

---
+++ start-loop640x480.sh
@@ -0,0 +1,10 @@
+#FRI="-r 15"
+FRI=-re
+FRO=""
+#FRO="-r 15"
+SZ=640x480
+while  [ 1 ] 
+do 
+   sleep 1
+   ffmpeg      $FRI      -i  09_30_26.264   $FRO    -s   $SZ     -c:v  libx264   -f  flv  rtmp://localhost/live/$SZ
+done 
