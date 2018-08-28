ddd71a388160881d1bcfcb743c110d7307d87548
root <you@example.com>
2018-08-28 02:41:42 +0000

    1

---
+++ v34/server/camera.h
@@ -0,0 +1,225 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include "tool.h"
+#include "videosource.h"
+#include "detectregion.h"
+class CameraInputData:public JsonData
+{
+public:
+    string Url;
+    vector <JsonPacket >DetectRegion;
+    CameraInputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    CameraInputData()
+    {
+    }
+    CameraInputData(vector <JsonPacket> regions,string url):DetectRegion(regions),Url(url)
+    {
+        encode();
+    }
+
+    void set_region(JsonPacket data,int index)
+    {
+        DetectRegion[index-1]=data;
+        encode();
+    }
+    void insert_region(JsonPacket data,int index)
+    {
+        vector <JsonPacket >::iterator begin=DetectRegion.begin();
+        DetectRegion.insert(begin+index,data);
+        encode();
+    }
+
+    void replace_point()
+    {
+
+    }
+    void decode()
+    {
+        DECODE_STRING_MEM(Url);
+        DECODE_OBJ_ARRAY_MEM(DetectRegion);
+    }
+    void encode()
+    {
+        // detect_regions.clear();
+        ENCODE_STRING_MEM(Url);
+        ENCODE_OBJ_ARRAY_MEM(DetectRegion);
+    }
+};
+class CameraOutputData:public JsonData
+{
+public:
+    vector<JsonPacket> DetectionResult;
+    int Timestamp;
+    CameraOutputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    CameraOutputData()
+    {
+    }
+    CameraOutputData(vector <JsonPacket> regions,int ts):DetectionResult(regions),Timestamp(ts)
+    {
+        encode();
+    }
+
+    //    void set_region(JsonPacket data,int index)
+    //    {
+    //        DetectRegion[index-1]=data;
+    //        encode();
+    //    }
+
+    //    void replace_point()
+    //    {
+
+    //    }
+    void decode()
+    {
+        DECODE_INT_MEM(Timestamp);
+        DECODE_OBJ_ARRAY_MEM(DetectionResult);
+    }
+    void encode()
+    {
+        ENCODE_INT_MEM(Timestamp);
+        ENCODE_OBJ_ARRAY_MEM(DetectionResult);
+    }
+};
+class Camera:public VdData<CameraInputData>
+{
+public:
+    enum OP{
+        CHANGE_URL,
+        INSERT_REGION,
+        DELETE_REGION,
+        MODIFY_REGION
+    };
+    function <void(Camera *,string)>callback_result;
+public:
+    Camera(JsonPacket cfg,function <void(Camera *,string)>fc):VdData(cfg),quit(false),callback_result(fc)
+    {
+        for(JsonPacket p:private_data.DetectRegion){
+            drs.push_back(new DetectRegion(p));
+        }
+        src=new VideoSource(private_data.Url);
+        start();
+    }
+    ~Camera()
+    {
+        prt(info,"exiting %s",private_data.Url.data());
+        quit=true;
+        if(work_trd->joinable())
+            work_trd->join();
+        prt(info,"exiting %s",private_data.Url.data());
+        delete work_trd;
+        for(DetectRegion *pro:drs)
+            delete pro;
+        drs.clear();
+        prt(info,"exited %s",private_data.Url.data());
+        delete src;
+    }
+
+    void run_process()
+    {
+        Mat frame;
+        int ts;
+        while(!quit){
+            this_thread::sleep_for(chrono::milliseconds(10));
+            if(src->get_frame(frame,ts)){
+#if 0
+                for(DetectRegion *r:drs){
+                    JsonPacket ret=r->work(frame);
+                    callback_result(this,ret.str());
+                }
+#endif
+                lock.lock();
+                vector<   JsonPacket >pkt;
+                int tmp=0;
+                for(DetectRegion *r:drs){
+                     prt(info,"region siz %d,now (%d) ",drs.size(),++tmp);
+                    JsonPacket ret=r->work(frame);
+                    pkt.push_back(ret);
+
+                }
+
+                CameraOutputData cod(pkt,ts);
+                timestamp=ts;
+                callback_result(this,cod.data().str());
+                lock.unlock();
+            }
+        }
+    }
+
+    void start()
+    {
+        //   _start(bind(&TestProcess::run_process,this));
+        //  _start(bind(&TestProcess::run_process,this,placeholders::_1),99);
+        //_start_async(bind(&Camera::run_process,this));
+        work_trd=new thread(bind(&Camera::run_process,this));
+        prt(info,"start done ~~~~~~~~~~~~~~~");
+    }
+
+    void change_source(string url)
+    {
+        if(src)
+            delete src;
+        src=new VideoSource(url);
+    }
+
+    void modify_detect_region(JsonPacket pkt)
+    {
+        int index=pkt.get("index").to_int();
+    }
+    int get_frame_ts()
+    {
+        return timestamp;
+    }
+    void modify(JsonPacket jp)
+    {
+        lock.lock();
+        RequestPkt req(jp);
+        int index=req.Index;
+        switch (req.Operation) {
+        case OP::CHANGE_URL:
+            change_source(req.Argument.get("Url").to_string());
+            break;
+        case OP::INSERT_REGION:
+        {
+            vector<DetectRegion*>::iterator it=drs.begin();
+            DetectRegion *rg=new DetectRegion(req.Argument);
+            drs.insert(it+index,rg);
+            private_data.insert_region(req.Argument,index);
+            break;
+        }
+        case OP::DELETE_REGION:
+        {
+            vector<DetectRegion*>::iterator it=drs.begin();
+            drs.erase(it+index-1);
+            break;
+        }
+        case OP::MODIFY_REGION:
+        {
+            vector<DetectRegion*>::iterator it=drs.begin();
+            DetectRegion *rg= drs[index-1];
+            rg->modify(req.Argument.get("ModifyRegion"));
+            private_data.set_region(rg->get_data().data(),index);
+            break;
+        }
+            break;
+        default:
+            break;
+        }
+        lock.unlock();
+    }
+
+private:
+    vector<DetectRegion*> drs;
+    VideoSource *src;
+    bool quit;
+    thread *work_trd;
+    int timestamp;
+    mutex lock;
+};
+
+#endif // CAMERA_H
---
+++ v34/server/videoprocessor.h
@@ -0,0 +1,135 @@
+#ifndef VIDEOPROCESSOR_H
+#define VIDEOPROCESSOR_H
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include "tool.h"
+#include "datapacket.h"
+using namespace std;
+using namespace cv;
+
+namespace VideoProcessorNS{
+typedef struct args{
+    double scale_ratio;
+    int scan_step;
+    Rect area;
+    int no;
+    string ratio;
+
+}arg_t;
+}
+using namespace VideoProcessorNS;
+class VideoProcessor
+{
+
+protected:
+    //  arg_t arg;
+public:
+    string alg_rst;
+#if 1
+    VideoProcessor()
+    {
+
+    }
+#else
+    VideoProcessor(DataPacket pkt):JsonDataDealer(pkt)
+    {
+
+    }
+#endif
+    int get_id()
+    {
+        return channel_id;
+    }
+    void set_config(DataPacket pkt)
+    {
+//        private_data.scale_ratio=string2f(pkt.get_string("ratio"));
+//        private_data.scan_step=pkt.get_int("step");
+//        vector <DataPacket> area=pkt.get_array_packet("detect_area");
+//        private_data.area=area_2_rect(area);
+//        private_data.area.x=0;
+//        private_data.area.y=0;
+//        private_data.area.width=640;
+//        private_data.area.height=480;
+    }
+
+
+
+    DataPacket get_config()
+    {
+        DataPacket pkt;
+//        pkt.set_int("step",private_data.scan_step);
+//        pkt.set_string("ratio",f2string(private_data.scale_ratio));
+        return pkt;
+    }
+//    virtual  bool process( Mat img)
+//    {
+//        return false;
+//    }
+//    virtual  bool process( int t)
+//    {
+//        return false;
+//    }
+//    virtual bool process(Mat img_src,vector<Rect> &rects)
+//    {
+
+//    }
+    virtual bool process(Mat img_src,JsonPacket &pkt)=0;
+//    {
+//        prt(info,"actual processor needed");
+//    }
+//    virtual bool process(Mat img_src,vector<Rect> &rects,Rect detect_area)
+//    {
+
+//        return false;
+//    }
+
+    virtual  string get_rst()
+    {
+        string ba;
+        return ba ;
+    }
+
+    virtual void init()
+    {
+    }
+
+    //    DataPacket get_config()
+    //    {
+    //        DataPacket ret;
+    //        encode(ret);
+    //        return ret;
+    //    }
+    Rect area_2_rect(vector<DataPacket> area)
+    {
+        int x_min=10000;
+        int y_min=10000;
+        int x_max=0;
+        int y_max=0;
+        for(DataPacket pkt: area) {
+            int x=pkt.get_int("x");
+            int y=pkt.get_int("y");
+            //              int x= v.toObject()["x"].toInt();
+            //            int y= v.toObject()["y"].toInt();
+            if(x<x_min)
+                x_min=x;
+            if(x>x_max)
+                x_max=x;
+            if(y<y_min)
+                y_min=y;
+            if(y>y_max)
+                y_max=y;
+        }
+        return Rect(x_min,y_min,x_max-x_min,y_max-y_min);
+    }
+
+protected:
+    int channel_id;
+private:
+
+};
+#endif // VIDEOPROCESSOR_H
