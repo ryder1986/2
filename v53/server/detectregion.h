312487f096434b4722a08be3eb682e4f280c238c
root <you@example.com>
2018-09-14 14:51:05 +0800

    1

---
+++ v53/server/detectregion.h
@@ -0,0 +1,247 @@
+#ifndef DETECTREGION_H
+#define DETECTREGION_H
+#include "jsonpacket.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include "videoprocessor.h"
+using namespace std;
+using namespace cv;
+#define LABLE_PROCESSOR_C4 "C4"
+#define LABLE_PROCESSOR_DUMMY "Dummy"
+
+#ifdef WITH_CUDA
+#define LABLE_PROCESSOR_PVD "Pvd"
+#define LABLE_PROCESSOR_FVD "Fvd"
+#define LABLE_PROCESSOR_MVD "Mvd"
+#endif
+class DetectRegionInputData:public JsonData
+{
+
+public:
+    string SelectedProcessor;
+    vector <VdPoint>ExpectedAreaVers;
+    JsonPacket ProcessorData;
+    DetectRegionInputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    DetectRegionInputData(JsonPacket pdata,string type,vector<VdPoint> vers)
+        :ProcessorData(pdata),SelectedProcessor(type),ExpectedAreaVers(vers)
+    {
+        encode();
+    }
+
+    void set_point(VdPoint p,int index)
+    {
+        ExpectedAreaVers[index-1]=p;
+        encode();
+    }
+
+    void set_points(vector <VdPoint> p)
+    {
+        ExpectedAreaVers=p;
+        encode();
+    }
+    void set_processor(string p,JsonPacket pd)
+    {
+        SelectedProcessor=p;
+        ProcessorData=pd;
+        encode();
+    }
+    void operator =(DetectRegionInputData dt)
+    {
+        config=dt.config;
+        decode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(ExpectedAreaVers);
+        DECODE_PKT_MEM(ProcessorData);
+        DECODE_STRING_MEM(SelectedProcessor);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(ExpectedAreaVers);
+        ENCODE_STRING_MEM(SelectedProcessor);
+        ENCODE_PKT_MEM(ProcessorData);
+    }
+
+};
+class ProcessorDataJsonData:public JsonData{
+public:
+    string SelectedProcessor;
+    JsonPacket ProcessorData;
+    ProcessorDataJsonData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    ProcessorDataJsonData(string pro,JsonPacket d)
+    {
+        ProcessorData=d;
+        SelectedProcessor=pro;
+        encode();
+    }
+    void decode()
+    {
+        DECODE_STRING_MEM(SelectedProcessor);
+        DECODE_PKT_MEM(ProcessorData);
+    }
+    void encode()
+    {
+        ENCODE_STRING_MEM(SelectedProcessor);
+        ENCODE_PKT_MEM(ProcessorData);
+    }
+};
+class AreaVersJsonData:public JsonData{
+public:
+    vector <VdPoint>ExpectedAreaVers;
+    AreaVersJsonData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    AreaVersJsonData(vector <VdPoint> vs)
+    {
+        ExpectedAreaVers.assign(vs.begin(),vs.end());
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(ExpectedAreaVers);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(ExpectedAreaVers);
+    }
+};
+class DetectRegionOutputData:public JsonData
+{
+public:
+    VdRect DetectionRect;
+    JsonPacket Result;
+    DetectRegionOutputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+
+    DetectRegionOutputData(JsonPacket rst ,VdRect rct)
+    {
+        DetectionRect=rct;
+        Result=rst;
+        encode();
+    }
+
+    void decode()
+    {
+        DECODE_PKT_MEM(DetectionRect);
+        DECODE_PKT_MEM(Result);
+    }
+
+    void encode()
+    {
+        ENCODE_JSONDATA_MEM(DetectionRect);
+        ENCODE_PKT_MEM(Result);
+    }
+
+};
+
+class DetectRegion : public VdData<DetectRegionInputData>
+{
+    int tmp;
+    VideoProcessor *p;
+    Rect detect_rect;
+    mutex lock;
+public:
+    enum OP{
+        CHANGE_RECT,
+        CHANGE_PROCESSOR
+    };
+
+
+    DetectRegion(DetectRegionInputData pkt);
+
+    ~DetectRegion()
+    {
+        if(p)
+            delete p;
+        p=NULL;
+    }
+
+    JsonPacket work(Mat frame)
+    {
+        lock.lock();
+        JsonPacket rst_r;
+        valid_rect(detect_rect,frame.cols,frame.rows);
+        if(detect_rect.width%2)detect_rect.width--;
+        if(detect_rect.height%2)detect_rect.height--;
+        Mat tmp=frame(detect_rect);
+        if(p &&detect_rect.x>=0&&detect_rect.x<10000
+                &&detect_rect.y>=0&&detect_rect.y<10000
+                &&detect_rect.width>0&&detect_rect.width<10000
+                &&detect_rect.height>0&&detect_rect.height<10000
+                &&frame.cols>0&&frame.rows>0
+                ){
+            p->process(tmp,rst_r);
+        }else{
+            prt(info,"err arg");
+        }
+        //  p->process(frame,rst_r);
+        VdRect r(detect_rect.x,detect_rect.y,detect_rect.width,detect_rect.height);
+        JsonPacket dct_rct=r.data();
+        DetectRegionOutputData rst(rst_r,dct_rct);
+        lock.unlock();
+        return rst.data();
+    }
+
+    void change_detector(string name)
+    {
+
+    }
+
+    void change_area( vector <VdPoint>poly_vers)
+    {
+        private_data.ExpectedAreaVers=poly_vers;
+    }
+
+    void modify(RequestPkt pkt);
+    bool process(JsonPacket data)
+    {
+
+    }
+private:
+    inline void valid_rect(Rect &area,int max_w,int max_h)
+    {
+        if((area.x+area.width)>max_w)
+            area.width=max_w-area.x;
+        if((area.y+area.height)>max_h)
+            area.height=max_h-area.y;
+    }
+    Rect reshape_2_rect(vector <VdPoint> area)
+    {
+        int x_min=10000;
+        int y_min=10000;
+        int x_max=0;
+        int y_max=0;
+        for(VdPoint pkt: area) {
+            int x=pkt.x;
+            int y=pkt.y;
+            if(x<x_min)
+                x_min=x;
+            if(x>x_max)
+                x_max=x;
+            if(y<y_min)
+                y_min=y;
+            if(y>y_max)
+                y_max=y;
+        }
+        x_min=x_min>0?x_min:0;
+        y_min=y_min>0?y_min:0;
+        x_max=x_max>0?x_max:0;
+        y_max=y_max>0?y_max:0;
+        return Rect(x_min,y_min,x_max-x_min,y_max-y_min);
+    }
+};
+
+#endif // DETECTREGION_H
---
+++ v53/server/videoprocessor.h
@@ -0,0 +1,642 @@
+#ifndef VIDEOPROCESSOR_H
+#define VIDEOPROCESSOR_H
+
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/video/video.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+
+#include "tool.h"
+#include "datapacket.h"
+using namespace std;
+using namespace cv;
+
+namespace VideoProcessorNS{
+typedef struct args{
+    double scale_ratio;
+    int scan_step;
+    Rect area;
+    int no;
+    string ratio;
+
+}arg_t;
+}
+using namespace VideoProcessorNS;
+class VideoProcessor
+{
+
+protected:
+    //  arg_t arg;
+public:
+    string alg_rst;
+#if 1
+    VideoProcessor()
+    {
+
+    }
+#else
+    VideoProcessor(DataPacket pkt):JsonDataDealer(pkt)
+    {
+
+    }
+#endif
+    virtual ~VideoProcessor()
+    {
+
+    }
+
+    int get_id()
+    {
+        return channel_id;
+    }
+    void set_config(DataPacket pkt)
+    {
+        //        private_data.scale_ratio=string2f(pkt.get_string("ratio"));
+        //        private_data.scan_step=pkt.get_int("step");
+        //        vector <DataPacket> area=pkt.get_array_packet("detect_area");
+        //        private_data.area=area_2_rect(area);
+        //        private_data.area.x=0;
+        //        private_data.area.y=0;
+        //        private_data.area.width=640;
+        //        private_data.area.height=480;
+    }
+
+
+
+    DataPacket get_config()
+    {
+        DataPacket pkt;
+        //        pkt.set_int("step",private_data.scan_step);
+        //        pkt.set_string("ratio",f2string(private_data.scale_ratio));
+        return pkt;
+    }
+    //    virtual  bool process( Mat img)
+    //    {
+    //        return false;
+    //    }
+    //    virtual  bool process( int t)
+    //    {
+    //        return false;
+    //    }
+    //    virtual bool process(Mat img_src,vector<Rect> &rects)
+    //    {
+
+    //    }
+    virtual bool process(Mat img_src,JsonPacket &pkt)=0;
+    //    {
+    //        prt(info,"actual processor needed");
+    //    }
+    //    virtual bool process(Mat img_src,vector<Rect> &rects,Rect detect_area)
+    //    {
+
+    //        return false;
+    //    }
+
+    virtual  string get_rst()
+    {
+        string ba;
+        return ba ;
+    }
+
+    virtual void init()
+    {
+    }
+
+    //    DataPacket get_config()
+    //    {
+    //        DataPacket ret;
+    //        encode(ret);
+    //        return ret;
+    //    }
+    Rect area_2_rect(vector<DataPacket> area)
+    {
+        int x_min=10000;
+        int y_min=10000;
+        int x_max=0;
+        int y_max=0;
+        for(DataPacket pkt: area) {
+            int x=pkt.get_int("x");
+            int y=pkt.get_int("y");
+            //              int x= v.toObject()["x"].toInt();
+            //            int y= v.toObject()["y"].toInt();
+            if(x<x_min)
+                x_min=x;
+            if(x>x_max)
+                x_max=x;
+            if(y<y_min)
+                y_min=y;
+            if(y>y_max)
+                y_max=y;
+        }
+        return Rect(x_min,y_min,x_max-x_min,y_max-y_min);
+    }
+
+protected:
+    int channel_id;
+private:
+
+};
+
+
+
+
+class DummyProcessorOutputData:public JsonData{
+
+public:
+    vector<VdPoint> Points;
+    int Radii;
+    DummyProcessorOutputData()
+    {
+    }
+    DummyProcessorOutputData(JsonPacket str):JsonData(str)
+    {
+        decode();
+    }
+    DummyProcessorOutputData(vector<VdPoint> o,int r):Radii(r)
+    {
+        Points.assign(o.begin(),o.end());
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(Points);
+        DECODE_INT_MEM(Radii);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(Points);
+        ENCODE_INT_MEM(Radii);
+    }
+};
+class DummyProcessorInputData:public JsonData{
+
+public:
+    bool Horizon;
+    bool Vertical;
+    int Radii;
+    DummyProcessorInputData(bool h,bool v,int r):Horizon(h),Vertical(v),Radii(r)
+    {
+        encode();
+    }
+    DummyProcessorInputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    DummyProcessorInputData()
+    {
+
+    }
+    void decode()
+    {
+        DECODE_BOOL_MEM(Horizon);
+        DECODE_BOOL_MEM(Vertical);
+        DECODE_INT_MEM(Radii);
+    }
+    void encode()
+    {
+        ENCODE_BOOL_MEM(Horizon);
+        ENCODE_BOOL_MEM(Vertical);
+        ENCODE_INT_MEM(Radii);
+    }
+};
+
+
+class C4ProcessorOutputData:public JsonData{
+
+public:
+    vector <VdRect> Rects;
+    C4ProcessorOutputData()
+    {
+    }
+    C4ProcessorOutputData(JsonPacket str):JsonData(str)
+    {
+        decode();
+    }
+    C4ProcessorOutputData(vector <VdRect> o):Rects(o)
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(Rects);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(Rects);
+    }
+};
+class C4ProcessorInputData:public JsonData{
+
+public:
+    double ratio;
+    int scan_step;
+    C4ProcessorInputData()
+    {
+    }
+    C4ProcessorInputData(JsonPacket str):JsonData(str)
+    {
+        decode();
+    }
+    C4ProcessorInputData(int st,double ra):scan_step(st),ratio(ra)
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_INT_MEM(scan_step);
+        DECODE_DOUBLE_MEM(ratio);
+    }
+    void encode()
+    {
+        ENCODE_INT_MEM(scan_step);
+        ENCODE_DOUBLE_MEM(ratio);
+    }
+};
+
+class PvdProcessorInputData:public JsonData{
+public:
+    vector <VdPoint> DetectLine;
+    PvdProcessorInputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    PvdProcessorInputData(VdPoint p1,VdPoint p2)
+    {
+        DetectLine.push_back(p1);
+        DetectLine.push_back(p2);
+        encode();
+    }
+    PvdProcessorInputData()
+    {
+
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(DetectLine);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(DetectLine);
+    }
+};
+
+class PvdProcessorOutputData:public JsonData{
+public:
+    vector <ObjectRect> PvdDetectedObjects;
+    int PersonFlow1;
+    int PersonFlow2;
+    int CurrentPersionCount;
+    PvdProcessorOutputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    PvdProcessorOutputData(    vector <ObjectRect> objs,
+                               int p1,
+                               int p2,
+                               int cnt)
+    {
+        PvdDetectedObjects.assign(objs.begin(),objs.end());
+        PersonFlow1=p1;
+        PersonFlow2=p2;;
+        CurrentPersionCount=cnt;
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(PvdDetectedObjects);
+        DECODE_INT_MEM(PersonFlow1);
+        DECODE_INT_MEM(PersonFlow2);
+        DECODE_INT_MEM(CurrentPersionCount);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(PvdDetectedObjects);
+        ENCODE_INT_MEM(PersonFlow1);
+        ENCODE_INT_MEM(PersonFlow2);
+        ENCODE_INT_MEM(CurrentPersionCount);
+    }
+};
+
+
+class BaseLineJsonData:public JsonData{
+public:
+    VdPoint BeginPoint;// start point
+    VdPoint EndPoint;// end point
+    int Length; // real length
+    BaseLineJsonData(VdPoint pt1, VdPoint pt2, int len):BeginPoint(pt1),EndPoint(pt2),Length(len)
+    {
+        encode();
+    }
+    BaseLineJsonData(JsonPacket p):JsonData(p)
+    {
+        decode();
+    }
+    BaseLineJsonData()
+    {
+
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_MEM(BeginPoint);
+        DECODE_JSONDATA_MEM(EndPoint);
+        DECODE_INT_MEM(Length);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_MEM(BeginPoint);
+        ENCODE_JSONDATA_MEM(EndPoint);
+        ENCODE_INT_MEM(Length);
+    }
+};
+class LaneDataJsonData:public JsonData{
+public:
+    int LaneNo;// lane name index;
+    vector <VdPoint> FarArea; // far rect
+    vector <VdPoint> NearArea; // near rect
+    vector <VdPoint> LaneArea; // whole rect
+    LaneDataJsonData(JsonPacket p):JsonData(p)
+    {
+        decode();
+    }
+    LaneDataJsonData(int ln, vector <VdPoint> fa,vector <VdPoint> na,vector <VdPoint> la):
+        LaneNo(ln),
+        FarArea(fa),
+        NearArea(na),
+        LaneArea(la)
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_INT_MEM(LaneNo);
+        DECODE_JSONDATA_ARRAY_MEM(FarArea);
+        DECODE_JSONDATA_ARRAY_MEM(NearArea);
+        DECODE_JSONDATA_ARRAY_MEM(LaneArea);
+
+    }
+    void encode()
+    {
+        ENCODE_INT_MEM(LaneNo);
+        ENCODE_JSONDATA_ARRAY_MEM(FarArea);
+        ENCODE_JSONDATA_ARRAY_MEM(NearArea);
+        ENCODE_JSONDATA_ARRAY_MEM(LaneArea);
+    }
+};
+class FvdProcessorInputData:public JsonData{
+public:
+    vector <VdPoint> BasicCoil;// standard rect
+    BaseLineJsonData BaseLine;// a line can adjust car real length
+    int NearPointDistance;//distance to camera
+    int FarPointDistance;
+    vector <LaneDataJsonData> LaneData; // lane info
+    FvdProcessorInputData(){}
+    FvdProcessorInputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    FvdProcessorInputData(  vector <VdPoint> bl,BaseLineJsonData be,int i1, int i2, vector <LaneDataJsonData>  ld):
+        BasicCoil(bl), BaseLine(be), NearPointDistance(i1), FarPointDistance(i2), LaneData(ld)
+
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(BasicCoil);
+        DECODE_JSONDATA_MEM(BaseLine);
+        DECODE_INT_MEM(NearPointDistance);
+        DECODE_INT_MEM(FarPointDistance);
+        DECODE_JSONDATA_ARRAY_MEM(LaneData);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(BasicCoil);
+        ENCODE_JSONDATA_MEM(BaseLine);
+        ENCODE_INT_MEM(NearPointDistance);
+        ENCODE_INT_MEM(FarPointDistance);
+        ENCODE_JSONDATA_ARRAY_MEM(LaneData);
+    }
+};
+class LaneOutputJsonData:public JsonData{
+public:
+    int LaneNo; // lane name index
+    int QueueLength;// length of queue
+    VdPoint StartQueuePoint;// queue start point
+    VdPoint EndQueuePoint;// queue end point
+    int LaneVehicleNumber;// lane car count now
+    int VehicleFlow;// flow total
+    int VehicleSpeed;// near rect car's speed
+    int NearActualLength;// near rect real size
+    int FarActualLength;// far rect real size
+    LaneOutputJsonData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    LaneOutputJsonData(int lo,int qh,VdPoint st,VdPoint et,int lr,int vw,int vd,int nh,int fh):
+        LaneNo(lo),
+        QueueLength(qh),
+        StartQueuePoint(st),
+        EndQueuePoint(et),
+        LaneVehicleNumber(lr),
+        VehicleFlow(vw),
+        VehicleSpeed(vd),
+        NearActualLength(nh),
+        FarActualLength(fh)
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_INT_MEM(LaneNo);
+        DECODE_INT_MEM(LaneNo);
+        DECODE_JSONDATA_MEM(StartQueuePoint);
+        DECODE_JSONDATA_MEM(EndQueuePoint);
+        DECODE_INT_MEM(LaneNo);
+        DECODE_INT_MEM(LaneNo);
+        DECODE_INT_MEM(LaneNo);
+        DECODE_INT_MEM(LaneNo);
+        DECODE_INT_MEM(LaneNo);
+
+
+    }
+    void encode()
+    {
+        ENCODE_INT_MEM(LaneNo);
+        ENCODE_INT_MEM(LaneNo);
+        ENCODE_JSONDATA_MEM(StartQueuePoint);
+        ENCODE_JSONDATA_MEM(EndQueuePoint);
+        ENCODE_INT_MEM(LaneNo);
+        ENCODE_INT_MEM(LaneNo);
+        ENCODE_INT_MEM(LaneNo);
+        ENCODE_INT_MEM(LaneNo);
+        ENCODE_INT_MEM(LaneNo);
+    }
+};
+class DegreeJsonData:public JsonData{
+public:
+    int DegreePointsY;
+    int DegreeValue;
+    DegreeJsonData(JsonPacket p):JsonData(p)
+    {
+        decode();
+    }
+    DegreeJsonData(int a,int b):DegreePointsY(a),DegreeValue(b)
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_INT_MEM(DegreePointsY);
+        DECODE_INT_MEM(DegreeValue);
+    }
+    void encode()
+    {
+        ENCODE_INT_MEM(DegreePointsY);
+        ENCODE_INT_MEM(DegreeValue);
+    }
+};
+class FvdProcessorOutputData:public JsonData{
+public:
+    vector <ObjectRect> FvdDetectedObjects;//all rects of car
+    int CurrentVehicleNumber; // cars number count on screen now;
+    int Visibility;// visiable or not
+    int VideoState;// video state
+    vector <LaneOutputJsonData> LaneOutputData;// output
+    vector <DegreeJsonData> DegreeData; // on  lane points
+
+    FvdProcessorOutputData(JsonPacket p):JsonData(p)
+    {
+        decode();
+    }
+    FvdProcessorOutputData(vector <ObjectRect> fs, int cr, int vy, int ve,  vector <LaneOutputJsonData> la,  vector <DegreeJsonData> da):
+        FvdDetectedObjects(fs),
+        CurrentVehicleNumber(cr),
+        Visibility(vy),
+        VideoState(ve),
+        LaneOutputData(la),
+        DegreeData(da)
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(FvdDetectedObjects);
+        DECODE_INT_MEM(CurrentVehicleNumber);
+        DECODE_INT_MEM(Visibility);
+        DECODE_INT_MEM(VideoState);
+        DECODE_JSONDATA_ARRAY_MEM(LaneOutputData);
+        DECODE_JSONDATA_ARRAY_MEM(DegreeData);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(FvdDetectedObjects);
+        ENCODE_INT_MEM(CurrentVehicleNumber);
+        ENCODE_INT_MEM(Visibility);
+        ENCODE_INT_MEM(VideoState);
+        ENCODE_JSONDATA_ARRAY_MEM(LaneOutputData);
+        ENCODE_JSONDATA_ARRAY_MEM(DegreeData);
+    }
+};
+
+
+class MvdProcessorInputData:public JsonData{
+public:
+    vector <VdPoint> BasicCoil;// standard rect
+    BaseLineJsonData BaseLine;// a line can adjust car real length
+    int NearPointDistance;//distance to camera
+    int FarPointDistance;
+    vector <LaneDataJsonData> LaneData; // lane info
+
+    vector <VdPoint> DetectLine;
+    MvdProcessorInputData(){}
+    MvdProcessorInputData(JsonPacket pkt):JsonData(pkt)
+    {
+        decode();
+    }
+    MvdProcessorInputData(  vector <VdPoint> bl,BaseLineJsonData be,int i1, int i2, vector <LaneDataJsonData>  ld, vector <VdPoint>de):
+        BasicCoil(bl), BaseLine(be), NearPointDistance(i1), FarPointDistance(i2), LaneData(ld),DetectLine(de)
+
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(BasicCoil);
+        DECODE_JSONDATA_MEM(BaseLine);
+        DECODE_INT_MEM(NearPointDistance);
+        DECODE_INT_MEM(FarPointDistance);
+        DECODE_JSONDATA_ARRAY_MEM(LaneData);
+        DECODE_JSONDATA_ARRAY_MEM(DetectLine);
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(BasicCoil);
+        ENCODE_JSONDATA_MEM(BaseLine);
+        ENCODE_INT_MEM(NearPointDistance);
+        ENCODE_INT_MEM(FarPointDistance);
+        ENCODE_JSONDATA_ARRAY_MEM(LaneData);
+        ENCODE_JSONDATA_ARRAY_MEM(DetectLine);
+    }
+};
+class MvdProcessorOutputData:public JsonData{
+public:
+    vector <ObjectRect> MvdDetectedObjects;//all rects of car
+    int CurrentVehicleNumber; // cars number count on screen now;
+    int Visibility;// visiable or not
+    int VideoState;// video state
+    vector <LaneOutputJsonData> LaneOutputData;// output
+    vector <DegreeJsonData> DegreeData; // on  lane points
+    int PersonFlow1;
+    int PersonFlow2;
+    int CurrentPersionCount;
+    MvdProcessorOutputData(JsonPacket p):JsonData(p)
+    {
+        decode();
+    }
+    MvdProcessorOutputData(vector <ObjectRect> fs, int cr, int vy, int ve,
+                           vector <LaneOutputJsonData> la,  vector <DegreeJsonData> da,int p1,int p2,int ct):
+        MvdDetectedObjects(fs),
+        CurrentVehicleNumber(cr),
+        Visibility(vy),
+        VideoState(ve),
+        LaneOutputData(la),
+        DegreeData(da),
+        PersonFlow1(p1),
+        PersonFlow2(p2),
+        CurrentPersionCount(ct)
+    {
+        encode();
+    }
+    void decode()
+    {
+        DECODE_JSONDATA_ARRAY_MEM(MvdDetectedObjects);
+        DECODE_INT_MEM(CurrentVehicleNumber);
+        DECODE_INT_MEM(Visibility);
+        DECODE_INT_MEM(VideoState);
+        DECODE_JSONDATA_ARRAY_MEM(LaneOutputData);
+        DECODE_JSONDATA_ARRAY_MEM(DegreeData);
+
+        DECODE_INT_MEM(PersonFlow1);
+        DECODE_INT_MEM(PersonFlow2);
+        DECODE_INT_MEM(CurrentPersionCount);
+
+    }
+    void encode()
+    {
+        ENCODE_JSONDATA_ARRAY_MEM(MvdDetectedObjects);
+        ENCODE_INT_MEM(CurrentVehicleNumber);
+        ENCODE_INT_MEM(Visibility);
+        ENCODE_INT_MEM(VideoState);
+        ENCODE_JSONDATA_ARRAY_MEM(LaneOutputData);
+        ENCODE_JSONDATA_ARRAY_MEM(DegreeData);
+
+        ENCODE_INT_MEM(PersonFlow1);
+        ENCODE_INT_MEM(PersonFlow2);
+        ENCODE_INT_MEM(CurrentPersionCount);
+    }
+};
+#endif // VIDEOPROCESSOR_H
